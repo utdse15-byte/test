ba4f3ee7d260abebed71f55891913cabce970e69
utdse15-byte <utdse15@gmail.com>
2026-07-15 06:54:53 -0700

    test(c87): pin BatchResult.canceled in to_dict for JobRunner

---
+++ tests/test_cycle87_batch_result_canceled_20260715.py
@@ -0,0 +1,7 @@
+﻿from manju.build.graph import BatchResult
+
+def test_batch_result_canceled_in_to_dict():
+    br = BatchResult(canceled=True, errors=["已取消"])
+    d = br.to_dict()
+    assert d.get("canceled") is True
+    assert d.get("errors") == ["已取消"]
